af4fad3fce078a46b3fcb8e01740be477d8810e2
Vitaly Egorov <egorov@samsonos.com>
2014-10-26 10:42:13 +0200

    Changed init class

---
+++ JSTreeView.php
@@ -0,0 +1,13 @@
+<?php
+namespace samson\js\treeview;
+
+/**
+ * SamsonPHP exteranl module
+ * @package SamsonJS
+ * @author Vitaly Iegorov <vitalyiegorov@gmail.com>
+ */
+class JSTreeView extends \samson\core\CompressableExternalModule
+{	
+	/** Identifier */
+	protected $id = 'treeview';
+}
